5e1a6588f5434200a585e706c2ccd81aebbb71b2
David Trower <dstrower@gmail.com>
2019-02-05 20:57:05 -0600

    Initial version

---
+++ zuul-proxy/src/main/java/com/vzw/eps/microservices/proxy/ZuulProxyApplication.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vzw.eps.microservices.proxy;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@RestController
+public class ZuulProxyApplication {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+	@RequestMapping("/timeout")
+    public String timeout() throws InterruptedException {
+        Thread.sleep(80000);
+        return "timeout";
+    }
+
+    @Bean
+    public ZuulFallbackProvider zuulFallbackProvider() {
+        return new ZuulFallbackProvider() {
+            @Override
+            public String getRoute() {
+                return "zuulserver";
+            }
+
+            @Override
+            public ClientHttpResponse fallbackResponse() {
+                return new ClientHttpResponse() {
+                    @Override
+                    public HttpStatus getStatusCode() throws IOException {
+                        return HttpStatus.OK;
+                    }
+
+                    @Override
+                    public int getRawStatusCode() throws IOException {
+                        return 200;
+                    }
+
+                    @Override
+                    public String getStatusText() throws IOException {
+                        return "OK";
+                    }
+
+                    @Override
+                    public void close() {
+
+                    }
+
+                    @Override
+                    public InputStream getBody() throws IOException {
+                        return new ByteArrayInputStream("fallback".getBytes());
+                    }
+
+                    @Override
+                    public HttpHeaders getHeaders() {
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.setContentType(MediaType.APPLICATION_JSON);
+                        return headers;
+                    }
+                };
+            }
+        };
+    }
+}
